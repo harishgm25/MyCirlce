15fcc9f9b1828bd40dc07d8cb9f7f760aec8d466
harish <harish.gm@gmail.com>
2015-03-15 07:33:57 +0530

    application rb modified

--- config/application.rb
+++ config/application.rb
@@ -66,7 +66,8 @@ module MyCircle
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
-
+   
     config.assets.initialize_on_precompile = false
+    config.autoload_paths += %W(#{config.root}/lib)
   end
 end
